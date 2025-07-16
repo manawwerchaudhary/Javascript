64bd4de86f01adc9ff729bcf6cfb32ecad406bca
Manawwer Chaudhary <111130626+manawwerchaudhary@users.noreply.github.com>
2025-07-16 18:01:32 +0000

    strings

---
+++ Basic/05_string.js
@@ -0,0 +1,12 @@
+// string interpoletion
+
+let name="manawwer"
+let age =23
+
+console.log(`i am ${name} and my age is ${age} and this is an example of string interpolation`)
+
+let name2=new String("Chaudhary")
+console.log(name2.toUpperCase())
+
+// read all methods of string
+// from mdn
