c3ab297e784b39076e5414a7061bbd86e5a400d2
Manawwer Chaudhary <111130626+manawwerchaudhary@users.noreply.github.com>
2025-07-16 16:29:18 +0000

    Conversion of datatypes

---
+++ Basic/03_conversionOperations.js
@@ -0,0 +1,10 @@
+let score="99"
+console.log(typeof score)
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)
+
+let number=""
+let result=Boolean(number)
+console.log(result)
