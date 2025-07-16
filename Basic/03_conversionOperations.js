f204606e928c83af9ca729752350bcaef91862f2
Manawwer Chaudhary <111130626+manawwerchaudhary@users.noreply.github.com>
2025-07-16 17:12:56 +0000

    datatypes summary

--- Basic/03_conversionOperations.js
+++ Basic/03_conversionOperations.js
@@ -8,3 +8,22 @@ console.log( valueInNumber)
 let number=""
 let result=Boolean(number)
 console.log(result)
+
+
+// array
+
+let array=[2,4,6,8]
+console.log(array)
+
+//symbol => stores uniques
+
+let id=Symbol(123)
+let anotherId=Symbol(123)
+console.log(id===anotherId)
+
+// function
+
+let myfunction=function(){
+    console.log("hello world")
+}
+myfunction();
