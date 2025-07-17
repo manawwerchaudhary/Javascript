d1e26ad5fb0a185de6dd527ad1abe7e13cebac78
Manawwer Chaudhary <111130626+manawwerchaudhary@users.noreply.github.com>
2025-07-17 08:56:09 +0000

    array_basic_01

---
+++ 02_basic/Array.js
@@ -0,0 +1,21 @@
+const arr=[1,2,3,4,5];
+console.log(arr);
+
+//to add element in array at last position
+arr.push(6)
+console.log(arr);
+
+arr.pop()
+console.log(arr);
+
+
+const arr2=new Array(1,2,6,7,8,9,0)
+console.log(arr2)
+
+const myArr1=arr2.slice(1,3)
+console.log(myArr1)
+
+const myArr2=arr2.splice(1,3)
+console.log(myArr2)
+console.log(arr2)
+
